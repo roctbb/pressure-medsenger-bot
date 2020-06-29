0b0afa1f0962740d24e4ddc9515e6e2f050bb0bc
Sergey Novozhilov <sergionov@mail.ru>
2020-06-29 16:56:51 +0300

    config.py --cached

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-HOST = "http://127.0.0.1:9099"
-MAIN_HOST = "http://localhost:8000"
-APP_KEY = "$2y$10$f5n5JMDCBiOW7MhPxbs9mumLDhCZuBBQJwBSkoKQRdrswyBly8PWu"
-SSL = ('cert.pem', 'key.pem')
-DEBUG = True
-
